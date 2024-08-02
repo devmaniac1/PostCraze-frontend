40e0d2dd7abd1541dde591eef58864b368981780
Ashfaq Farleen <139785309+devmaniac1@users.noreply.github.com>
2024-08-02 08:58:12 +0530

    Update PostDetail.js

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -20,7 +20,7 @@ function PostDetail() {
     );
 
   return (
-    <div className="w-9/12 my-6 mx-auto md:w-6/12">
+    <div className="w-full my-6 mx-auto md:w-6/12 px-4">
       <Button to="-1">{"Back"}</Button>
       <div className="mt-6 flex flex-col gap-4">
         <Post post={post} />
